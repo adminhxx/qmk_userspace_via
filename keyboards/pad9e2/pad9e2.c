2285b8450b3c86a811c5d65bfc5680fd2ccad925
xuqingzhong <13588196563@163.com>
2026-01-15 17:40:28 +0800

    fix:test

---
+++ keyboards/pad9e2/pad9e2.c
@@ -0,0 +1,1 @@
+#include "pad9e2.h"
---
+++ keyboards/pad9e2/pad9e2.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "quantum.h"
+
+#define LAYOUT( \
+    k_e1p, k_e2p, \
+    k02, k03, k04, \
+    k05, k06, k07, \
+    k08, k09, k10  \
+) { \
+    { k_e1p, k_e2p, k02, k03, k04, k05, k06, k07, k08, k09, k10 } \
+}
